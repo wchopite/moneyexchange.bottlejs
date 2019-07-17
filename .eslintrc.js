824db8214504120a5f76f07a36a56c8d41408966
Wladimir Chopite <wchopite@gmail.com>
2019-07-17 15:08:00 -0300

    [first commit]

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "eslint:recommended",
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "ecmaVersion": 6,
+      "experimentalObjectRestSpread": true
+    },
+    "sourceType": "module"
+  }
+};
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Ok' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
